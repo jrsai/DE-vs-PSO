3062e0fd869573fa3e2fb84778534edf9e7eb091
Shivanouyen Yoganathan <shivanouyen.yoganathan@uoit.net>
2017-11-26 11:33:21 -0500

    Update Functions.java

--- PSO-Algorithm/src/Functions.java
+++ PSO-Algorithm/src/Functions.java
@@ -1,10 +1,10 @@
 public class Functions {
-	public static final double LOC_X_LOW = 1;
-	public static final double LOC_X_HIGH = 4;
-	public static final double LOC_Y_LOW = -1;
-	public static final double LOC_Y_HIGH = 1;
-	public static final double VEL_LOW = -1;
-	public static final double VEL_HIGH = 1;
+	public static final double LOC_X_LOW = -10;
+	public static final double LOC_X_HIGH = 10;
+	public static final double LOC_Y_LOW = -10;
+	public static final double LOC_Y_HIGH = 10;
+	public static final double VEL_LOW = -10;
+	public static final double VEL_HIGH = 10;
 	
 	public static final double ERR_TOLERANCE = 1E-20; // the smaller the tolerance, the more accurate the result, 
 	                                                  // but the number of iteration is increased
